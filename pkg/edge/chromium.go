d50c86f93b58419c6afc4504753774c27c0d0aa3
Anderson <80022991+Humphryyy@users.noreply.github.com>
2024-09-03 11:32:07 -0700

    Update imports

--- pkg/edge/chromium.go
+++ pkg/edge/chromium.go
@@ -16,8 +16,8 @@ import (
 	"time"
 	"unsafe"
 
-	"github.com/wailsapp/go-webview2/internal/w32"
-	"github.com/wailsapp/go-webview2/webviewloader"
+	"github.com/AlpineAIO/go-webview2/internal/w32"
+	"github.com/AlpineAIO/go-webview2/webviewloader"
 	"golang.org/x/sys/windows"
 )
 
